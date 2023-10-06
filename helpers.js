baf8e00ce37679ba7cac8aa57897b7cf8d9a98a2
Marc Schär <marcschaer@Marcs-MacBook-Pro.local>
2023-10-06 11:18:32 +0200

    README

--- helpers.js
+++ helpers.js
@@ -66,23 +66,6 @@ function convertAndValidateNums(numsString) {
   return numsArray;
 }
 
-// function convertAndValidateNumsArray(numsAsStrings) {
-//   let result = [];
-
-//   for (let i = 0; i < numsAsStrings.length; i++) {
-//     let valToNumber = Number(numsAsStrings[i]);
-
-//     if (Number.isNaN(valToNumber)) {
-//       return new Error(
-//         `The value '${numsAsStrings[i]}' at index ${i} is not a valid number.`
-//       );
-//     }
-
-//     result.push(valToNumber);
-//   }
-//   return result;
-// }
-
 module.exports = {
   findMean,
   findMedian,
